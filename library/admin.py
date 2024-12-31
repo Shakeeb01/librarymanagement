1864db12db5e678522419304a60fa3be7e4243e6
Shakeeb ur Rehman <104270729+Shakeeb01@users.noreply.github.com>
2024-12-31 17:27:16 +0500

    Update admin.py
    Admin Class Modified

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Book,StudentExtra,IssuedBook
+
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = [] # here we will pass the list of model fields that we want to display in admin panel.
+    list_editable = [] # this will give the permission to edit specific fields.
+    
 admin.site.register(Book, BookAdmin)
 
 
